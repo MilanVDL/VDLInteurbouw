ecc956ab004b28b1b8b44a815f15add1beb2d911
MilanVDL <milan.vanderLinden@student.hu.nl>
2023-12-11 17:18:00 +0100

    Update Footer
    gkjnre

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,7 +9,7 @@ const navigation = {
     {
       name: "Facebook",
       href: "#",
-      icon: (props) => (
+      icon: (props: any) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
